9fb0b26631e2d36127fc1bb34db38f58e3548e38
Pierre-Henry Soria <hi@ph7.me>
2021-10-24 18:56:14 +1030

    Add Gravatar image generator

---
+++ src/Gravatar/Image.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ph7\PH2Gravatar\Gravatar;
+
+final class Image
+{
+    private const DEFAULT_SIZE = 200;
+    private const DEFAULT_RATING = 'g';
+    private const DEFAULT_DISPLAY = 'mp';
+    private const GRAVATAR_URL = 'https://www.gravatar.com';
+
+    public static function get(string $email, array $options = []): string
+    {
+        $url = self::GRAVATAR_URL . '/avatar/' . self::getEmailHash($email);
+
+        return self::buildUrl($url, self::buildOptions($options));
+    }
+
+    private static function getEmailHash(string $email): string
+    {
+        return md5(strtolower($email));
+    }
+
+    private static function buildUrl(string $url, string $params): string
+    {
+        return $url . '?' . $params;
+    }
+
+    private static function buildOptions(array $options): string
+    {
+        $params = [
+            's' => !empty($options['size']) ? $options['size'] : self::DEFAULT_SIZE,
+            'r' => !empty($options['rating']) ? $options['rating'] : self::DEFAULT_RATING,
+            'd' => !empty($options['display']) ? $options['display'] : self::DEFAULT_DISPLAY,
+        ];
+
+        return http_build_query($params);
+    }
+}
