9fe9113d270aa7db20ddef39e28a6b9a2e1e17cc
Pierre-Henry Soria <hi@ph7.me>
2021-10-24 18:56:28 +1030

    Add unit tests :smiley:

---
+++ tests/Gravatar/ImageTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ph7\PH2Gravatar\Tests\Gravatar;
+
+use Ph7\PH2Gravatar\Gravatar\Image;
+use PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase
+{
+    private const TEST_EMAIL = 'test@ph7.me';
+    private const TEST_EMAIL_HASH = '25b846cb8825d0854bd7a24792f8945a';
+
+    public function testGravatarWithoutOptions(): void
+    {
+        $actual = Image::get(self::TEST_EMAIL);
+        $expected = 'https://www.gravatar.com/avatar/' . self::TEST_EMAIL_HASH . '?s=200&r=g&d=mp';
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testGravatarWithoutOptionsAndUppercaseEmail(): void
+    {
+        $upperCaseEmail = strtoupper(self::TEST_EMAIL);
+        $actual = Image::get($upperCaseEmail);
+        $expected = 'https://www.gravatar.com/avatar/' . self::TEST_EMAIL_HASH . '?s=200&r=g&d=mp';
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testGravatarWithOptions(): void
+    {
+        $upperCaseEmail = strtoupper(self::TEST_EMAIL);
+        $actual = Image::get($upperCaseEmail, [
+            'size' => 400,
+            'rating' => 'pg',
+            'display' => 'retro'
+        ]);
+        $expected = 'https://www.gravatar.com/avatar/' . self::TEST_EMAIL_HASH . '?s=400&r=pg&d=retro';
+
+        $this->assertSame($expected, $actual);
+    }
+}
